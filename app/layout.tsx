8a2eeca6ab7e4cf94bf97cf041d42e92a3aaafcf
rhuddlestone <rhuddlestone@me.com>
2025-01-06 07:14:06 +0700

    Add analytics to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,9 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <Navbar />
-          <main className="pt-16">{children}</main>
+          <main className="pt-16">
+            {children} <Analytics />
+          </main>
         </body>
       </html>
     </ClerkProvider>
