5ff06a909044e45a8257aa64e5c9eda6bd251445
rhuddlestone <rhuddlestone@me.com>
2025-01-06 07:10:31 +0700

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ClerkProvider } from '@clerk/nextjs';
+import { Analytics } from "@vercel/analytics/react";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
 
@@ -8,7 +9,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "PRD Creator - Streamline Your Project Requirements",
-  description: "Efficiently create Project Requirement Documents tailored for indie hackers and solopreneurs.",
+  description:
+    "Efficiently create Project Requirement Documents tailored for indie hackers and solopreneurs.",
 };
 
 export default function RootLayout({
@@ -21,9 +23,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <Navbar />
-          <main className="pt-16">
-            {children}
-          </main>
+          <main className="pt-16">{children}</main>
         </body>
       </html>
     </ClerkProvider>
